27ae8e9fdc5402fa0098f3a72702dd01e0738ba6
fg-alin <fg.alin@yahoo.com>
2022-05-30 21:57:13 +0300

    First commit

---
+++ src/main/java/com/application/dlibrary/DlibraryApplication.java
@@ -0,0 +1,13 @@
+package com.application.dlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlibraryApplication.class, args);
+	}
+
+}
